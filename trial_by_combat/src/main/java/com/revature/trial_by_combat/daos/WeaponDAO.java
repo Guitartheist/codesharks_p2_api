c3b145d74f20c7c4157e8078a6ed33d664acdc91
Philip Wentworth <guitartheist@gmail.com>
2022-03-03 08:50:49 -0500

    quick fix to weaponDAO, added @Query annotation

--- trial_by_combat/src/main/java/com/revature/trial_by_combat/daos/WeaponDAO.java
+++ trial_by_combat/src/main/java/com/revature/trial_by_combat/daos/WeaponDAO.java
@@ -2,6 +2,7 @@ package com.revature.trial_by_combat.daos;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,6 @@ import com.revature.trial_by_combat.models.Weapon;
 
 @Repository
 public interface WeaponDAO extends CrudRepository<Weapon, Integer> {
-
+	@Query("from Item i where i.itemname=:itemname")
     Optional<Weapon> findWeaponByItemName(String itemname);
 }
